1852b624df5258095b920b64c524b184fac975aa
baifengliu <734055580@qq.com>
2019-12-13 14:06:25 +0800

    增加上一篇下一篇功能

--- src/main/java/com/mt/portalcms/mapper/CmsArticleMapper.java
+++ src/main/java/com/mt/portalcms/mapper/CmsArticleMapper.java
@@ -2,6 +2,7 @@ package com.mt.portalcms.mapper;
 
 import com.mt.portalcms.common.config.MyMapper;
 import com.mt.portalcms.pojo.CmsArticle;
+import tk.mybatis.mapper.additional.idlist.IdListMapper;
 
-public interface CmsArticleMapper extends MyMapper<CmsArticle> {
+public interface CmsArticleMapper extends MyMapper<CmsArticle> , IdListMapper<CmsArticle,Long> {
 }
